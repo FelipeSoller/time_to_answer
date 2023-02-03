490bebac6ac62aebbee8091a27691b902c728bd1
Felipe Soller <soller@outlook.com>
2023-02-03 10:51:25 -0300

    create edit and update enpoint for admins

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,4 +2,19 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
     @admins = Admin.all
   end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+
+  def update
+    @admin = Admin.find(params[:id])
+    admin_params = params.require(:admin).permit(:email, :password, :password_confirmation)
+
+    if @admin.update(admin_params)
+      redirect_to admins_backoffice_admins_path, notice: 'Administrador alterado com sucesso'
+    else
+      render :edit
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
   namespace :site do
     get 'welcome/index'
   end
@@ -12,6 +9,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index'
+    resources :admins, only: %i[index edit update]
   end
 
   devise_for :admins
