8650123801aa0f8a8258e82b2910b80a20ba662b
Felipe Soller <soller@outlook.com>
2023-03-02 18:06:38 -0300

    refactor to show a message in jumbotron container

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def jumbotron_message
+    case params[:action]
+    when 'index'
+      'Últimas perguntas cadastradas'
+    when 'questions'
+      "Exibindo resultados para o termo \"#{params[:term]}\""
+    when 'subject'
+      "Exibindo questões de assunto: \"#{params[:subject]}\""
+    end
+  end
 end
