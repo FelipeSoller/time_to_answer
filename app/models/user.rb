5f0a7f6e989d36cca7396097f1f35b7aa28f46b5
Felipe Soller <soller@outlook.com>
2023-03-11 13:24:22 -0300

    add validates in first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true, on: :update
 
   def full_name
     [self.first_name, self.last_name].join(' ')
