37a174abeeb964c227c105acbbbb6058b252e220
Felipe Soller <soller@outlook.com>
2023-03-19 21:09:40 -0300

    set statistics for questions and users

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,9 +3,17 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  after_create :set_statistic
+
   paginates_per 10
 
   scope :_search_subject, -> (subject_id, page) { includes(:answers, :subject).where(subject_id: subject_id).page(page) }
   scope :_search, -> (term, page) { includes(:answers, :subject).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page) }
   scope :last_questions, -> (page) { Question.includes(:answers).order('created_at desc').page(page) }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,17 @@ class User < ApplicationRecord
   has_one :user_profile
   accepts_nested_attributes_for :user_profile, reject_if: :all_blank
 
+  after_create :set_statistic
+
   validates :first_name, :last_name, presence: true, on: :update
 
   def full_name
     [self.first_name, self.last_name].join(' ')
+  end
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_users])
   end
 end
